e412c71dbedb1a9d799d99d9ecc57dc8a4153fb6
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2024-11-10 19:13:14 -0600

    more dependency err fixes

--- app/ass/page.tsx
+++ app/ass/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import ObjectDetection from "@/components/AI/ObjectDetection";
-import OcrUpload from "@/components/AI/OCRUpload";
 import ChevronsUpDown from "@/components/icons/ChevronsUpDown";
 import { Button } from "@/components/ui/button";
 import ItsDropdown from "@/components/ui/its-dropdown";
 import TransitionX from "@/components/ui/TransitionX";
 import React from "react";
 import { BsThreeDots } from "react-icons/bs";
+import OcrUpload from "@/components/AI/OcrUpload";
 
 const page = () => {
   return (
---
+++ app/object-detection/page.tsx
@@ -0,0 +1,8 @@
+import ObjectDetection from "@/components/AI/ObjectDetection";
+import React from "react";
+
+const Page = () => {
+  return <ObjectDetection />;
+};
+
+export default Page;
