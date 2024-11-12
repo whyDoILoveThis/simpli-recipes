25764aba0d75198b7e43e6f587d6a881eb083a4a
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2024-11-12 06:07:08 -0600

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ["firebasestorage.googleapis.com", 'img.clerk.com'], // Add the Clerk image domain here
+      domains: ["firebasestorage.googleapis.com", 'img.clerk.com', "images.unsplash.com"], // Add the Clerk image domain here
     },
     webpack: (config) => {
       config.module.rules.push({
